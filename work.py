a8438098eb69f6d65a946a8058bc02c615a03186
Ekaterina <KaterinaCV284@yandex.ru>
2023-07-05 17:54:13 +0300

    Update work.py
    доработки

--- work.py
+++ work.py
@@ -4,11 +4,17 @@ import datetime
 know = datetime.date.today() #дата (с отсечением времени).
 monthYou = int(input('В каком месяце вы устроились? '))
 
+сur_year = know.year # Год текущий
 cur_month = know.month # Месяц текущий
 b = cur_month - monthYou
 know = know.replace(month = monthYou) # меняем только месяц
-
-print('Вы работаете:', b, 'месяц. Устроились', know.strftime('%m.%Y'))
+if monthYou > cur_month:
+    yearYou = int(input('Введите год в котором вы устроились: '))
+    know.year - yearYou
+    b = cur_month + (12 - monthYou)
+    print('Вы работаете:', b, 'месяц. Устроились', monthYou,'.',yearYou)#.strftime('%m.%Y'))
+else:
+    print('Вы работаете:', b, 'месяц. Устроились', know.strftime('%m.%Y'))
 
 payday = 50000
 indexPay = payday + (b * 100)
