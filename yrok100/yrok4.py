002e3b896a3bb4a6719c71dfcdb0abb528c239bc
Ekaterina <KaterinaCV284@yandex.ru>
2023-07-10 20:44:30 +0300

    новая папка yrok
    Задания на уроке

---
+++ yrok100/yrok4.py
@@ -0,0 +1,12 @@
+snachala = 'Было слово'
+potom = 'получился текст'
+
+sohranim = snachala
+snachala += potom
+print(snachala)
+
+# Каковы значения переменных? 
+# Что случилось с переменной "snachala"? - Было словополучился текст
+print('\x6d\x61\x6d\x61 \x6d\x79\x6c\x61 \x72\x61\x6d\x75')
+# mama myla ramu
+
